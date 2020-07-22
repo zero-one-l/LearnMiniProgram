14e005c185c7686e0d8c081387af0bc5b7a70f11
zero-one-l <1691775662@qq.com>
2020-07-22 09:44:19 +0800

    WXS

---
+++ page/wxs/wxs.js
@@ -0,0 +1,15 @@
+// page/wxs/wxs.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+   price: 25.66666,
+   time: 1560598160
+  },
+  numbertoFixed(value){
+    return value.foFixed(2)
+  }
+  
+})
