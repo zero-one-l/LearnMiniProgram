ac823b1125c6eccc9fb5fa617bf705f35b074d65
zero-one-l <1691775662@qq.com>
2020-07-23 09:50:03 +0800

    弹窗和分享

--- page/home/home.js
+++ page/home/home.js
@@ -1,66 +1,57 @@
 // page/home/home.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  handleShowToast(){
+    wx.showToast({
+      title: '加载中...',
+      //停留时间
+      duration: 3000,
+      icon: 'loading',
+      mask: true
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleShowModal(){
+    wx.showModal({
+      title: '我是标题',
+      content: '我是内容，哈哈哈哈',
+      //不要取消按钮
+      //showCancel: false,
+      //将取消改成别的文字
+     //cancelText: '退出',
+      success(res){
+        if(res.cancel){
+          console.log('用户点击了取消按钮')
+        }
+        if(res.confirm){
+          console.log('用户点击了确定按钮')
+        }
+      }
+    })
   },
+  handleShowLoading(){
+    wx.showLoading({
+      title: '加载ing',
+      mask: true
+    })
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+    setTimeout(()=>{
+      //必须手动调用hideLoading才会让loading消失
+      wx.hideLoading()
+    },1000)
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleShowAction(){
+    wx.showActionSheet({
+      itemList: ['相册','拍照'],
+      itemColor: 'red',
+      success:function(res){
+        console.log(res)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onShareAppMessage:function(options){
+    return {
+      title: '你好啊',
+      path: '/page/about/about.wxml',
+      imageUrl: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1595477780680&di=e88102d8e4fcc36326b34b9a1ff24cff&imgtype=0&src=http%3A%2F%2Fa1.att.hudong.com%2F62%2F02%2F01300542526392139955025309984.jpg'
+    }
   }
 })
